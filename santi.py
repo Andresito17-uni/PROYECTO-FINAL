212f0fbce5cd26f8867eb6d2b6059ebacb8541e1
Andresito17-uni <Andrescarvajal261@gmail.com>
2026-04-20 10:44:38 -0500

    Zero

--- santi.py
+++
@@ -1,7 +0,0 @@
-print("me arrepiento de haber dicho proyecto")
-
-if 1 == 1:
-    print("esto es un if")
-else:    print("esto es un else")
-lo que quiera = "esto es una variable"
-print(lo que quiera)
